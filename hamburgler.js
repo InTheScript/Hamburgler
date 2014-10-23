049e04962b4b887f6427ad0abe8ac00a8fb72cf8
Mulenga Agley <mulengaagley@gmail.com>
2014-10-23 19:13:35 +0100

    First

---
+++ hamburgler.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".icon").click(function () {
+        $(".mobilenav").fadeToggle(500);
+        $(".top-menu").toggleClass("top-animate");
+        $(".mid-menu").toggleClass("mid-animate");
+        $(".bottom-menu").toggleClass("bottom-animate");
+    });
+});
